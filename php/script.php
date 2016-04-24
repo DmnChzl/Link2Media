924936cd895d85cd3475e89e890fb0e790832d4d
MrDoomy <damien.chazoule@gmail.com>
2016-04-24 17:00:18 +0200

    Fix

--- php/script.php
+++ php/script.php
@@ -9,7 +9,7 @@
     $command = 'youtube-dl -o "/home/link2media/Music.%(ext)s" -x --audio-format mp3 ' .escapeshellarg($link);
   } else {
     $log['filename'] = 'Video.mp4';
-    $command = 'youtube-dl -o "/home/link2media/Music.%(ext)s" -f mp4 ' .escapeshellarg($link);
+    $command = 'youtube-dl -o "/home/link2media/Video.%(ext)s" -f mp4 ' .escapeshellarg($link);
   }
 
   if (exec($command, $output, $ret)) {
