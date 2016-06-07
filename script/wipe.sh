40a3dfb0323355fbabde22426b14654d4489c8d3
MrDoomy <damien.chazoule@gmail.com>
2016-06-07 19:57:03 +0200

    Minor update

--- script/wipe.sh
+++ script/wipe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo find /home/link2media/* -mtime +7 -type f -delete
-echo "At $(date +'%H:%M on %d/%m/%Y'), $(ls /home/link2media/ | wc -l) deteled file(s)." > log.txt
+echo "At $(date +'%H:%M on %d/%m/%Y'), $(ls /home/link2media/ | wc -l) remaining file(s)." > /var/www/link2media/script/log.txt
