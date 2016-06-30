0e8c06bd53da36d38f0ecd9b5aa650ce4b472ed0
MrDoomy <damien.chazoule@gmail.com>
2016-06-30 09:50:22 +0200

    Update download script

--- php/download.php
+++ php/download.php
@@ -1,21 +1,39 @@
 <?php
   include("config.php");
-  $filename = $_POST['filename'];
-  $file = $folder.$filename;
-  // $ext = pathinfo($filename, PATHINFO_EXTENSION);
 
-  if (file_exists($file)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/stream-octet');
-    header("Content-Transfer-Encoding: binary");
-    header('Content-Disposition: attachment; filename="'.$filename.'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' .filesize($file));
-    readfile($file);
-    // unlink($file);
-  } else {
-    header("HTTP/1.0 404 Not Found");
+  ignore_user_abort(true);
+  set_time_limit(0); // Disable the time limit for this script
+
+  $filename = $_POST['filename'];
+  $path = $folder.$filename;
+
+  if ($input = fopen($path, "r")) {
+    $size = filesize($path);
+    $info = pathinfo($path);
+    $ext = strtolower($info["extension"]);
+    switch ($ext) {
+      case "mp3":
+      header("Content-type: application/mp3");
+      header("Content-Disposition: attachment; filename=\"".$info["basename"]."\""); // Use 'attachment' to force a file download
+      break;
+      case "mp4":
+      header("Content-type: application/mp4");
+      header("Content-Disposition: attachment; filename=\"".$info["basename"]."\""); // Use 'attachment' to force a file download
+      break;
+      // Add more headers for other content types here
+      default;
+      header("Content-type: application/octet-stream");
+      header("Content-Disposition: filename=\"".$info["basename"]."\"");
+      break;
+    }
+    header("Content-length: $size");
+    header("Cache-control: private"); // Use this to open files directly
+    while(!feof($input)) {
+      $buffer = fread($input, 2048);
+      echo $buffer;
+    }
   }
+  fclose ($input);
+  exit;
+
 ?>
